aa0db9e1fc0b32bfc74e37426ecee69d41567b88
Barath Subramaniam <baraths84@gmail.com>
2024-10-20 20:09:59 -0700

    Update utils.ts

--- src/hooks/utils.ts
+++ src/hooks/utils.ts
@@ -2,7 +2,13 @@ import { Client } from "@langchain/langgraph-sdk";
 
 export const createClient = () => {
   const apiUrl = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000/api";
+  const apiKey = process.env.LANGCHAIN_API_KEY;
+   if (!apiKey) {
+    throw new Error("LANGCHAIN_API_KEY environment variable is not set");
+  }
+
   return new Client({
     apiUrl,
+    apiKey,
   });
 };
